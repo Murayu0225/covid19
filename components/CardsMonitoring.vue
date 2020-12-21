5348445c3615e4eb3a710dfdaaf935f99ff0fa8c
Yu Muramatsu <yu.murama2@gmail.com>
2020-12-21 11:46:45 +0900

    Update(v0.2.3)201221

--- components/CardsMonitoring.vue
+++ components/CardsMonitoring.vue
@@ -8,15 +8,13 @@ import Vue from 'vue'
 
 // 検査陽性者の状況
 const ConfirmedCasesDetailsCard = () =>
-  import('@/components/cards/ConfirmedCasesDetailsCard.vue'),
+  import('@/components/cards/ConfirmedCasesDetailsCard.vue')
 /* eslint-enable simple-import-sort/sort */
 
 export default Vue.extend({
   data() {
     return {
-      rows: [
-        [ConfirmedCasesDetailsCard],
-      ],
+      rows: [[ConfirmedCasesDetailsCard]],
     }
   },
 })
