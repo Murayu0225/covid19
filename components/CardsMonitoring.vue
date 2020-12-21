86bbe806efc2954da55355b35e14144f82a53a9a
Assistant-App <68503487+Assistant-App@users.noreply.github.com>
2020-12-21 09:33:57 +0900

    Update(v0.1.9)201221

--- components/CardsMonitoring.vue
+++ components/CardsMonitoring.vue
@@ -9,27 +9,6 @@ import Vue from 'vue'
 // 検査陽性者の状況
 const ConfirmedCasesDetailsCard = () =>
   import('@/components/cards/ConfirmedCasesDetailsCard.vue')
-// 報告日別による陽性者数の推移
-const ConfirmedCasesNumberCard = () =>
-  import('@/components/cards/ConfirmedCasesNumberCard.vue')
-// モニタリング項目(1)新規陽性者数
-const MonitoringConfirmedCasesNumberCard = () =>
-  import('@/components/cards/MonitoringConfirmedCasesNumberCard.vue')
-// 発症日別による陽性者数の推移
-const PositiveNumberByDevelopedDateCard = () =>
-  import('@/components/cards/PositiveNumberByDevelopedDateCard.vue')
-// 確定日別による陽性者数の推移
-const PositiveNumberByDiagnosedDateCard = () =>
-  import('@/components/cards/PositiveNumberByDiagnosedDateCard.vue')
-// 検査実施件数
-const TestedNumberCard = () => import('@/components/cards/TestedNumberCard.vue')
-// モニタリング項目(4)検査の陽性率
-const PositiveRateCard = () => import('@/components/cards/PositiveRateCard.vue')
-// モニタリング項目(6)入院患者数
-const HospitalizedNumberCard = () =>
-  import('@/components/cards/HospitalizedNumberCard.vue')
-// モニタリング項目(7)重症患者数
-const SevereCaseCard = () => import('@/components/cards/SevereCaseCard.vue')
 /* eslint-enable simple-import-sort/sort */
 
 export default Vue.extend({
