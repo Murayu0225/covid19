3784dcc37938457172e464fe2b78256f8a99775e
Murayu0225 <68503487+Murayu0225@users.noreply.github.com>
2020-12-22 16:49:52 +0900

    fix program

--- components/MonitoringItemsOverviewTableMedicalSystem.vue
+++ components/MonitoringItemsOverviewTableMedicalSystem.vue
@@ -4,48 +4,48 @@
       <div :class="$style.content">
         <span>{{ $t('検査体制') }}</span>
       </div>
-    </li>
-
-    <li :class="[$style.box, $style.parent]">
-      <div :class="$style.content">
-        <span>{{ $t('陽性率') }}</span>
-        <monitoring-items-overview-table-value-with-translatable-unit
-          :value="items['陽性率'].value"
-          :unit="items['陽性率'].unit"
-        />
-      </div>
       <ul :class="$style.group">
-        <li :class="[$style.box]">
+        <li :class="[$style.box, $style.parent]">
           <div :class="$style.content">
-            <span>{{ $t('陽性率（参考値）') }}</span>
-            <span>{{ items['陽性率（参考値）'].value }}</span>
+            <span>{{ $t('陽性率') }}</span>
+            <monitoring-items-overview-table-value-with-translatable-unit
+              :value="items['陽性率'].value"
+              :unit="items['陽性率'].unit"
+            />
           </div>
         </li>
-      </ul>
+          <ul :class="$style.group">
+            <li :class="[$style.box]">
+              <div :class="$style.content">
+                <span>{{ $t('陽性率（参考値）') }}</span>
+                <span>{{ items['陽性率（参考値）'].value }}</span>
+              </div>
+            </li>
+          </ul>
     </li>
     <li :class="[$style.box, $style.parent]">
       <div :class="$style.content">
         <span>{{ $t('受入体制') }}</span>
       </div>
-    </li>
-
-    <li :class="[$style.box, $style.parent]">
-      <div :class="$style.content">
-        <span>{{ $t('入院患者数') }}</span>
-        <monitoring-items-overview-table-value-with-translatable-unit
-          :value="items['入院患者数'].value"
-          :unit="items['入院患者数'].unit"
-        />
-      </div>
       <ul :class="$style.group">
-        <li :class="[$style.box]">
+        <li :class="[$style.box, $style.parent]">
           <div :class="$style.content">
-            <span>{{ $t('入院患者数（参考値）') }}</span>
-            <span>{{ items['入院患者数（参考値）'].value }}</span>
+            <span>{{ $t('入院患者数') }}</span>
+            <monitoring-items-overview-table-value-with-translatable-unit
+              :value="items['入院患者数'].value"
+              :unit="items['入院患者数'].unit"
+            />
           </div>
         </li>
-      </ul>
-    </li>
+          <ul :class="$style.group">
+            <li :class="[$style.box]">
+              <div :class="$style.content">
+                <span>{{ $t('入院患者数（参考値）') }}</span>
+                <span>{{ items['入院患者数（参考値）'].value }}</span>
+              </div>
+            </li>
+          </ul>
+      </li>
   </ul>
 </template>
 
