df6f9dd94d12b6822ed4c70d202d083d438279d1
Murayu0225 <68503487+Murayu0225@users.noreply.github.com>
2020-12-22 23:21:29 +0900

    スマートフォンでの表示を改善

--- components/CardsReference.vue
+++ components/CardsReference.vue
@@ -9,20 +9,14 @@ import Vue from 'vue'
 // 陽性者の属性
 const ConfirmedCasesAttributesCard = () =>
   import('@/components/cards/ConfirmedCasesAttributesCard.vue')
-// 発症日別による陽性者数の推移
-const PositiveNumberByDevelopedDateCard = () =>
-  import('@/components/cards/PositiveNumberByDevelopedDateCard.vue')
-// 確定日別による陽性者数の推移
-const PositiveNumberByDiagnosedDateCard = () =>
-  import('@/components/cards/PositiveNumberByDiagnosedDateCard.vue')
-// 検査実施件数
-const TestedNumberCard = () => import('@/components/cards/TestedNumberCard.vue')
+// モニタリング状況
+const MonitoringItemsOverviewCard = () =>
+  import('@/components/cards/MonitoringItemsOverviewCard.vue')
 /* eslint-enable simple-import-sort/sort */ export default Vue.extend({
   data() {
     return {
       rows: [
-        [ConfirmedCasesAttributesCard, PositiveNumberByDevelopedDateCard],
-        [PositiveNumberByDiagnosedDateCard, TestedNumberCard],
+        [ConfirmedCasesAttributesCard, MonitoringItemsOverviewCard],
       ],
     }
   },
