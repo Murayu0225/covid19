e8ca9833dcddbd099ee756487aaf6f47cb1437e3
Murayu0225 <68503487+Murayu0225@users.noreply.github.com>
2020-12-23 15:05:52 +0900

    Update
    Co-Authored-By: Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>

--- components/SiteTopUpper.vue
+++ components/SiteTopUpper.vue
@@ -16,7 +16,6 @@
       </div>
     </div>
     <whats-new class="mb-4" :items="newsItems" :is-emergency="false" />
-    <monitoring-comment-card />
     <lazy-tokyo-alert-card v-if="TokyoAlert.alert" />
     <lazy-static-info
       v-if="$vuetify.breakpoint.smAndUp || showStaticInfo"
@@ -33,7 +32,6 @@ import { mdiChartTimelineVariant } from '@mdi/js'
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
 
-import MonitoringCommentCard from '@/components/MonitoringCommentCard.vue'
 import PageHeader from '@/components/PageHeader.vue'
 import WhatsNew from '@/components/WhatsNew.vue'
 import Data from '@/data/data.json'
@@ -45,7 +43,6 @@ export default Vue.extend({
   components: {
     PageHeader,
     WhatsNew,
-    MonitoringCommentCard,
   },
   data() {
     const { lastUpdate } = Data
