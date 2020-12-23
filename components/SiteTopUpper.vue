385d47fc53cee99ffa12b5fc50bd52b56a47e75f
Murayu0225 <68503487+Murayu0225@users.noreply.github.com>
2020-12-23 13:20:19 +0900

    Update SiteTopUpper.vue
    Co-Authored-By: Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>

--- components/SiteTopUpper.vue
+++ components/SiteTopUpper.vue
@@ -16,12 +16,12 @@
       </div>
     </div>
     <whats-new class="mb-4" :items="newsItems" :is-emergency="false" />
-    <!--<monitoring-comment-card />-->
-    <!--<lazy-tokyo-alert-card v-if="TokyoAlert.alert" />-->
+    <monitoring-comment-card />
+    <lazy-tokyo-alert-card v-if="TokyoAlert.alert" />
     <lazy-static-info
       v-if="$vuetify.breakpoint.smAndUp || showStaticInfo"
       class="mb-4"
-      :url="'https://www.city.sagamihara.kanagawa.jp/shisei/koho/1019896.html'"
+      :url="'https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/coronasodan.html'"
       :text="$t('自分や家族の症状に不安や心配があればまずは電話相談をどうぞ')"
       :btn-text="$t('相談の手順を見る')"
     />
@@ -32,22 +32,26 @@
 import { mdiChartTimelineVariant } from '@mdi/js'
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
+import MonitoringCommentCard from '@/components/MonitoringCommentCard.vue'
 import PageHeader from '@/components/PageHeader.vue'
 import WhatsNew from '@/components/WhatsNew.vue'
 import Data from '@/data/data.json'
 import News from '@/data/news.json'
+import TokyoAlert from '@/data/tokyo_alert.json'
 import { convertDatetimeToISO8601Format } from '@/utils/formatDate'
 export default Vue.extend({
   components: {
     PageHeader,
     WhatsNew,
+    MonitoringCommentCard,
   },
   data() {
     const { lastUpdate } = Data
     return {
+      TokyoAlert,
       headerItem: {
         iconPath: mdiChartTimelineVariant,
-        title: this.$t('市内の最新感染動向'),
+        title: this.$t('都内の最新感染動向'),
       },
       lastUpdate,
       newsItems: News.newsItems,
@@ -69,7 +73,7 @@ export default Vue.extend({
   },
   head(): MetaInfo {
     return {
-      title: this.$t('市内の最新感染動向') as string,
+      title: this.$t('都内の最新感染動向') as string,
     }
   },
 })
