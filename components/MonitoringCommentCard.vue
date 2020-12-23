5dfd78172d682536cf864ccc08d6d7ad3325fd6e
Murayu0225 <68503487+Murayu0225@users.noreply.github.com>
2020-12-23 23:43:00 +0900

    Update MonitoringCommentCard.vue
    Co-Authored-By: Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>

--- components/MonitoringCommentCard.vue
+++ components/MonitoringCommentCard.vue
@@ -9,7 +9,7 @@
       <p>
         {{
           $t(
-            '当サイトは、毎日18時〜22時の間に更新しています。更新日時を確認する場合には、上の最新更新時間を確認してください。下記リンクの方が、情報更新が速いことがありますのでそちらもご確認ください。'
+            '当サイトは、毎日18時〜22時の間に更新しています。更新日時を確認する場合には、上の最新更新時間を確認してください。下記リンクの方が、情報更新が速いことがありますのでこちらもご確認ください。'
           )
         }}
       </p>
@@ -28,37 +28,15 @@ import { mdiChevronRight } from '@mdi/js'
 import Vue from 'vue'
 
 import AppLink from '@/components/AppLink.vue'
-import MonitoringCommentFrame from '@/components/MonitoringCommentFrame.vue'
-import monitoringItemsData from '@/data/monitoring_items.json'
-import {
-  formatMonitoringComment,
-  MonitoringComment,
-} from '@/utils/formatMonitoringItems'
-
-type CommentKey = {
-  [key: string]: MonitoringComment
-}
 
 export default Vue.extend({
   components: {
     AppLink,
-    MonitoringCommentFrame,
   },
   data() {
-    const monitoringComment: CommentKey = formatMonitoringComment(
-      monitoringItemsData.data
-    )
     return {
-      monitoringComment,
       mdiChevronRight,
     }
-  },
-  methods: {
-    commentMonitoring(item: string) {
-      return ['ja', 'ja-basic'].includes(this.$root.$i18n.locale)
-        ? this.monitoringComment[item].display['@ja']
-        : this.monitoringComment[item].display['@en']
-    },
   },
 })
 </script>
