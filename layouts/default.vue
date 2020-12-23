661b8ff2166e72adfb8c072adeb6526b5a5fd775
Yu Muramatsu <yu.murama2@gmail.com>
2020-12-23 11:10:11 +0900

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -179,6 +179,7 @@ export default Vue.extend({
           name: 'apple-mobile-web-app-title',
           content: `${this.$t('相模原市')} ${this.$t(
             '新型コロナウイルス感染症'
+            '新型コロナウイルス感染症'
           )} ${this.$t('対策サイト')}`,
         },
         {
