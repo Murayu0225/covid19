bff1b231165ca56aa45d4d1282753f3b2fdedc8c
Murayu0225 <yu.murama2@gmail.com>
2021-03-02 23:16:08 +0000

    fix lint error

--- pages/contacts.vue
+++ pages/contacts.vue
@@ -62,26 +62,29 @@
                   >
                 </li>
                 <li>
-                  {{ $t('外国人専用ダイヤル（対応言語：英語、中国語、韓国語）') }}<br /><a href="tel:042-767-2104"
-                    >042-767-2104</a
-                  ><br />
-              <p class="caution">
-                {{ $t(`午前8時30から午後7時まで`) }}
-              </p>
-              <p class="caution">
-                {{ $t(`土・日、祝日も含む`) }}
-              </p>
-              <p class="caution">
-                {{ $t(`詳しくは、{pdf}をご覧ください。`) }}
-              </p>
-                <template v-slot:pdf>
-                  <app-link
-                    :to="$t('https://www.city.sagamihara.kanagawa.jp/_res/projects/default_project/_page_/001/019/191/02/0226.pdf')"
-                    :icon-size="16"
-                  >
-                    {{ $t('こちら(PDF 104.7KB)') }}
-                  </app-link>
-                </template>
+                  {{ $t('外国人専用ダイヤル（対応言語：英語、中国語、韓国語）')
+                  }}<br /><a href="tel:042-767-2104">042-767-2104</a><br />
+                  <p class="caution">
+                    {{ $t(`午前8時30から午後7時まで`) }}
+                  </p>
+                  <p class="caution">
+                    {{ $t(`土・日、祝日も含む`) }}
+                  </p>
+                  <p class="caution">
+                    {{ $t(`詳しくは、{pdf}をご覧ください。`) }}
+                  </p>
+                  <template v-slot:pdf>
+                    <app-link
+                      :to="
+                        $t(
+                          'https://www.city.sagamihara.kanagawa.jp/_res/projects/default_project/_page_/001/019/191/02/0226.pdf'
+                        )
+                      "
+                      :icon-size="16"
+                    >
+                      {{ $t('こちら(PDF 104.7KB)') }}
+                    </app-link>
+                  </template>
                 </li>
               </ul>
             </td>
