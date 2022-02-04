335bc0510ccd9137c7d0e2ed1256eb6fde7dbfac
Yu Muramatsu <murayu@assistant-app.com>
2022-02-04 11:22:51 +0900

    delete module files (.nuxt)

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['redirect'] = require('..\\middleware\\redirect.ts')
-middleware['redirect'] = middleware['redirect'].default || middleware['redirect']
-
-export default middleware
