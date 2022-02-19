88915b19eff8cf92582ae56a8cf1566be730beff
Murayu0225 <Murayu0225@users.noreply.github.com>
2022-02-19 11:52:54 +0000

    deploy: 5ebf2b57c276e7697512514f3893c1bd562949d4

---
+++ _nuxt/static/1645271552/en/cards/positive-number-over70/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/en/cards/positive-number-over70", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1645271552/ko/cards/positive-number-over70/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/ko/cards/positive-number-over70", {data:[{}],fetch:{},mutations:[]});
